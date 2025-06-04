85380c93f5f625ed30db8d16f1cca20b597102b0
Yuthapump <you@example.com>
2025-06-04 15:24:06 +0700

    Refactor environment variable checks for improved clarity

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,22 +2,17 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const formLink = process.env.GOOGLE_FORM_LINK;
-  const fieldIdName = process.env.GOOGLE_FORM_FIELD_ID_NAME;
-  const fieldIdEmail = process.env.GOOGLE_FORM_FIELD_ID_EMAIL;
-  const fieldIdMessage = process.env.GOOGLE_FORM_FIELD_ID_MESSAGE;
-  const fieldIdSocial = process.env.GOOGLE_FORM_FIELD_ID_SOCIAL;
-
-  if (
-    !formLink ||
-    !fieldIdName ||
-    !fieldIdEmail ||
-    !fieldIdMessage ||
-    !fieldIdSocial
-  ) {
+  if (!formLink) {
     return new NextResponse("Please configure the env variables", {
       status: 500,
     });
   }
+
+  // configure this according to your google form
+  const fieldIdName = process.env.GOOGLE_FORM_FIELD_ID_NAME;
+  const fieldIdEmail = process.env.GOOGLE_FORM_FIELD_ID_EMAIL;
+  const fieldIdMessage = process.env.GOOGLE_FORM_FIELD_ID_MESSAGE;
+  const fieldIdSocial = process.env.GOOGLE_FORM_FIELD_ID_SOCIAL;
 
   try {
     const body = await req.json();
